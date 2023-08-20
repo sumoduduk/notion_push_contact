1e50077f13db1b0583e262401e1e0a3dc7f35534
Iqbal Abdurachman <sumoduduk13@gmail.com>
2023-08-20 01:36:44 -0500

    axum home

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_rust));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on {addr}");
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hello_rust() -> String {
+    "Hello Rust Axum".to_owned()
 }
