1188a0a4e6100c11fd2c3dab8bd8c1b95270d541
Iqbal Abdurachman <sumoduduk13@gmail.com>
2023-08-21 22:59:52 -0500

    add another router

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
 mod router;
 
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
+use router::push_contact;
 use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello_rust));
+    let cors = CorsLayer::new()
+        .allow_methods(Any)
+        .allow_headers(Any)
+        .allow_origin(Any);
+
+    let app = Router::new()
+        .route("/", get(hello_rust))
+        .route("/contact_form", post(push_contact))
+        .layer(cors);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("Listening on {addr}");
@@ -17,5 +30,5 @@ async fn main() {
 }
 
 async fn hello_rust() -> String {
-    "Hello Rust Axum".to_owned()
+    "Hello World Rust Axum".to_owned()
 }
--- src/router/notion_contact.rs
+++ src/router/notion_contact.rs
@@ -5,11 +5,17 @@ use std::env;
 
 pub async fn push_notion(value: Value) -> Result<Value, reqwest::Error> {
     let notion_url = env::var("NOTION_URL").expect("NOTION_URL key not found");
+    let bearer = env::var("BEARER").expect("BEARER not found");
+
+    let bearer = format!("Bearer {bearer}");
 
     let client = Client::new();
 
     let resp: Value = client
         .post(notion_url)
+        .header("Authorization", bearer)
+        .header("Content-Type", "application/json")
+        .header("Notion-Version", "2022-06-28")
         .json(&value)
         .send()
         .await?
@@ -36,7 +42,7 @@ pub fn convert_to_json(contact: &ContactPayload) -> Value {
                 },
               ],
             },
-            "Email": {
+            "email": {
               "email": contact.email,
             },
             "Message": {
@@ -49,11 +55,9 @@ pub fn convert_to_json(contact: &ContactPayload) -> Value {
               ],
             },
             "Contacted": {
-              "select": {
-                "name": "false",
+               "checkbox": false
               },
-            },
-          },
+       },
     });
     payload
 }
